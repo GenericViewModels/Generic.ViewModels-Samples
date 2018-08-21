ffdf8a1ffed177520eeeb3152e7f9ea615340def
Christian Nagel <christian@christiannagel.com>
2018-08-21 22:10:33 +0200

    add xamarin list sample

---
+++ BooksSample/BooksApp_Xamarin/BooksApp_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using BooksLib.ViewModels;
+
+namespace BooksApp_Xamarin
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            //Appearing += OnAppearing;
+            //Disappearing += OnDisappearing;
+
+            //_scope = AppServices.Instance.ServiceProvider.CreateScope();
+            //ViewModel = _scope.ServiceProvider.GetRequiredService<BooksViewModel>();
+            InitializeComponent();
+        }
+
+        //private void OnAppearing(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void OnDisappearing(object sender, EventArgs e)
+        //{
+        //    _scope?.Dispose();
+        //    _scope = null;
+
+        //    Disappearing += OnDisappearing;
+        //}
+
+       // public BooksViewModel ViewModel { get; private set; }
+        
+    }
+}
---
+++ BooksSample/BooksApp_Xamarin/BooksApp_Xamarin/Views/BooksPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BooksLib.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BooksApp_Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BooksPage : ContentPage
+    {
+        private IServiceScope _scope;
+
+        public BooksPage()
+        {
+            InitializeComponent();
+            _scope = AppServices.Instance.ServiceProvider.CreateScope();
+
+            ViewModel = _scope.ServiceProvider.GetRequiredService<BooksViewModel>();
+
+            BindingContext = this;
+
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        public BooksViewModel ViewModel { get; private set; }
+    }
+}
